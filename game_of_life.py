8d97d54e16b5f01b8f304cafd6645778857c4ae1
KennTheCodingSaiyan <jiananyu0011@gmail.com>
2018-05-17 18:04:43 -0400

    rand implemented

--- game_of_life.py
+++ game_of_life.py
@@ -1,6 +1,6 @@
 import gui_game_board
 import pygame
-
+import random
 BOARD_WIDTH = 1920
 BOARD_HEIGHT = 1080
 
@@ -10,6 +10,15 @@ DEAD_CHAR = "*"
 
 class GameOfLife(object):
     def __init__(self):
+
+
+        print("given a 192 x 108 board, you want to indicate coordinate for where live is created")
+        w = int(input("please enter the width:"))
+        h = int(input("please enter the height:"))
+
+        bit_vector = self.bit_vector_init(w, h)
+
+        self.brd_file_init(bit_vector)
         pygame.init()
         size = [BOARD_WIDTH, BOARD_HEIGHT]
         screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
@@ -20,6 +29,62 @@ class GameOfLife(object):
         self.gboard = gui_game_board.GUIGameBoard(screen,
                                                   test_map_path,
                                                   test_board_path)
+
+    def bit_vector_init(self, width, height):
+        bit_vector = []
+        for x in range(192*108):
+            bit_vector.append(0)
+
+        if width <= 10:
+            width += 10
+        elif width >= 192 - 10:
+            width -= 10
+
+        if height <= 10:
+            height += 10
+        elif height >= 108 - 10:
+            height -= 10
+
+        random.seed()
+
+        for x in range(random.randint(0, 500)):
+            nh = height + random.randint(-4, 4)
+            nw = width + random.randint(-4, 4)
+
+            if nh >= 192:
+                continue
+            elif nw >= 108:
+                continue
+
+            bit_vector[nh*nw] = 1
+
+        return bit_vector
+
+    def brd_file_init(self, bit_vector):
+        fd = open("boards/default.brd", 'w')
+        w = int(BOARD_WIDTH) // 10
+        h = int(BOARD_HEIGHT) // 10
+        fd.write(str(w) + '\n')
+        fd.write(str(h) + '\n')
+
+        i = 0
+        j = 0
+        count = 0
+        while i < h:
+            wline = ""
+            while j < w:
+                if bit_vector[count] != 0:
+                    wline += '-'
+                else:
+                    wline += '*'
+                count += 1
+                j += 1
+            fd.write(wline + '\n')
+            i += 1
+            j = 0
+
+        fd.close()
+
 
     def animation(self):
         done = False
